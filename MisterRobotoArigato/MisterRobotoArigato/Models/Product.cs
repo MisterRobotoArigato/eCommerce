91f96a92badc284b231461fe84f52ce94940699b
jcqnly <jcqnly@gmail.com>
2018-07-09 22:47:27 -0700

    add properties to Product class

---
+++ MisterRobotoArigato/MisterRobotoArigato/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
