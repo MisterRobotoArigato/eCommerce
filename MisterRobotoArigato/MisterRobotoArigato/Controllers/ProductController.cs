a284f2b83602cbfa0994db4dee476b8efc08a323
ecaoile <unfie555@gmail.com>
2018-07-10 10:00:26 -0700

    added construtor to product controller

---
+++ MisterRobotoArigato/MisterRobotoArigato/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MisterRobotoArigato.Data;
+
+namespace MisterRobotoArigato.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RobotoDbContext _context;
+        private readonly IConfiguration Configuration;
+
+        public ProductController(RobotoDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
