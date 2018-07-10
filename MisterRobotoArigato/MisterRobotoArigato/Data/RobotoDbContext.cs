a0df62b74002534ff9fa91ee067f904fe3ff79de
jcqnly <jcqnly@gmail.com>
2018-07-09 22:24:38 -0700

    add Configuration for db connection

---
+++ MisterRobotoArigato/MisterRobotoArigato/Data/RobotoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Data
+{
+    public class RobotoDbContext : DbContext
+    {
+        public RobotoDbContext(DbContextOptions<RobotoDbContext> options) : base (options)
+        {
+
+        }
+
+
+    }
+}
--- MisterRobotoArigato/MisterRobotoArigato/Startup.cs
+++ MisterRobotoArigato/MisterRobotoArigato/Startup.cs
@@ -5,17 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MisterRobotoArigato.Data;
 
 namespace MisterRobotoArigato
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<RobotoDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
