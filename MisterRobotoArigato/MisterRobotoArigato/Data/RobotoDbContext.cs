f3bbf12412e444c9b4899be7d3a786fe2fb89b2b
ecaoile <unfie555@gmail.com>
2018-07-10 09:40:10 -0700

    added sql database with products table

--- MisterRobotoArigato/MisterRobotoArigato/Data/RobotoDbContext.cs
+++ MisterRobotoArigato/MisterRobotoArigato/Data/RobotoDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MisterRobotoArigato.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace MisterRobotoArigato.Data
 
         }
 
+        DbSet<Product> Products { get; set; }
     }
 }
