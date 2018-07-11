73b1b267ac3afce7e391b82a81bc094e901bc9ce
ecaoile <unfie555@gmail.com>
2018-07-11 14:01:35 -0700

    completed first user story

---
+++ MisterRobotoArigato/MisterRobotoArigato/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MisterRobotoArigato.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
