3a536d925282054c23fead36d5eaecd693f2f025
jcqnly <jcqnly@gmail.com>
2018-07-10 18:46:51 -0700

    add SeedData.cs and add connection to Program.cs to initialize the file upon load

--- MisterRobotoArigato/MisterRobotoArigato/Data/RobotoDbContext.cs
+++ MisterRobotoArigato/MisterRobotoArigato/Data/RobotoDbContext.cs
@@ -14,6 +14,6 @@ namespace MisterRobotoArigato.Data
 
         }
 
-        DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- MisterRobotoArigato/MisterRobotoArigato/Program.cs
+++ MisterRobotoArigato/MisterRobotoArigato/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MisterRobotoArigato.Data;
+using MisterRobotoArigato.Models;
+using System;
 
 namespace MisterRobotoArigato
 {
@@ -14,7 +13,25 @@ namespace MisterRobotoArigato
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<RobotoDbContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB. Sad face.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
