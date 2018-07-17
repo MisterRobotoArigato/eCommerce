c43497a25a99bbd27bab761bc3448546fba9234c
ecaoile <unfie555@gmail.com>
2018-07-17 13:03:59 -0700

    stopping point (add to cart button still doesn't work)

---
+++ MisterRobotoArigato/MisterRobotoArigato/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MisterRobotoArigato.Models;
+using MisterRobotoArigato.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Controllers
+{
+    [Authorize]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepo _repo;
+        private readonly IConfiguration Configuration;
+
+        public BasketController(IBasketRepo repo, IConfiguration configuration)
+        {
+            _repo = repo;
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string email, Product product)
+        {
+            _repo.AddProductToBasket(email, product);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
