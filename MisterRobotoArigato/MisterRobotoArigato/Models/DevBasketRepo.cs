5dcfc9273b8967aa3258198f43725c0158e800d0
ecaoile <unfie555@gmail.com>
2018-07-17 11:30:36 -0700

    created basket interface, wrote methods for grabbing basket and adding item to basket

---
+++ MisterRobotoArigato/MisterRobotoArigato/Models/DevBasketRepo.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MisterRobotoArigato.Data;
+using MisterRobotoArigato.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Models
+{
+    public class DevBasketRepo : IBasketRepo
+    {
+        private RobotoDbContext _context;
+
+        public DevBasketRepo(RobotoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<HttpStatusCode> AddProductToBasket(string email, Product product)
+        {
+            try
+            {
+                Basket datBasket = GetUserBasketByEmail(email).Result;
+                bool basketHasItem = datBasket.Products.Contains(product);
+                if (basketHasItem == true)
+                {
+                    await UpdateBasket(email, datBasket);
+                    return HttpStatusCode.Created;
+                }
+
+                BasketDetail datBD = new BasketDetail();
+                datBD.ProductID = product.ID;
+                datBD.CustomerEmail = email;
+                datBD.Quantity = 1;
+                datBD.UnitPrice = product.Price;
+
+                await _context.AddAsync(datBD);
+                await _context.SaveChangesAsync();
+                return HttpStatusCode.Created;
+            }
+
+            catch
+            {
+                return HttpStatusCode.BadRequest;
+            }
+        }
+
+        public Task<HttpStatusCode> DeleteProductFromBasket(string email, Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Basket> GetUserBasketByEmail(string email)
+        {
+            var prodInts = _context.BasketDetails.Where(d => d.CustomerEmail == email).Select(p => p.ProductID);
+            List<Product> demProducts = _context.Products.Where(p => prodInts.Contains(p.ID)).ToList();
+            Basket datBasket = await _context.Baskets.FirstOrDefaultAsync(b => b.CustomerEmail == email);
+            datBasket.Products = demProducts;
+            return datBasket;
+        }
+
+        public Task<Basket> UpdateBasket(string email, Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MisterRobotoArigato/MisterRobotoArigato/Models/Interfaces/IBasketRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MisterRobotoArigato.Models.Interfaces
+{
+    public interface IBasketRepo
+    {
+        Task<HttpStatusCode> AddProductToBasket(string email, Product product);
+
+        Task<Basket> GetUserBasketByEmail(string email);
+
+        Task<Basket> UpdateBasket(string email, Basket basket);
+
+        Task<HttpStatusCode> DeleteProductFromBasket(string email, Basket basket);
+    }
+}
