6fd47bf29fa036d7dee83dcdfc5d6b5ca2a636eb
jcqnly <jcqnly@gmail.com>
2018-08-19 16:05:59 -0700

    fix startup.cs config

--- MisterRobotoArigato/MisterRobotoArigato/Startup.cs
+++ MisterRobotoArigato/MisterRobotoArigato/Startup.cs
@@ -82,19 +82,6 @@ namespace MisterRobotoArigato
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
-            //app.UseMvc(route =>
-            //{
-            //    route.MapRoute(
-            //        name: "default",
-            //        template: "{controller=Home}/{action=Index}/{id?}");
-            //});
-
-            //app.Run((context) =>
-            //{
-            //    context.Response.Redirect("/");
-            //    return Task.FromResult<object>(null);
-            //});
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("errrrrrooooooooooooor!");
